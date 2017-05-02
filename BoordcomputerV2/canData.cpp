a08c2a8458badf4d532d72ea3d10ae314dc6e373
Jo <jo.laridon@student.vives.be>
2017-05-02 13:17:07 +0200

    recreated candata class

--- BoordcomputerV2/canData.cpp
+++ BoordcomputerV2/canData.cpp
@@ -2,16 +2,7 @@
 
 #include "canData.h"
 
-CanData::CanData(int value,QString description,int id,QString raw,int timestamp)
-{
-    /*! \todo create class constructor */
-    Description = description;
-    ID = id;
-    RAW = raw;
-    Value = value;
-    Timestamp = timestamp;
 
-}
 CanData::CanData()
 {
 
--- BoordcomputerV2/canData.h
+++ BoordcomputerV2/canData.h
@@ -10,17 +10,33 @@
 
 class CanData
 {
+private:
 
 
-    public:
-    QString Description;
-    int ID;
-    QString RAW;
-    int Value;
-    int Timestamp;
 
-        CanData(int,QString,int,QString,int);
-        CanData();
+    QString description;
+    int id;
+    QString raw;
+    int value;
+    int timestamp;
+
+public:
+
+     QString Description(){return description;}
+     int ID(){return id;}
+     QString RAW(){return raw;}
+     int Value(){return value;}
+     int Timestamp(){return timestamp;}
+
+     void SetDescription(QString setDescription){description = setDescription;}
+     void SetID(int setId){id = setId;}
+     void SetRAW(QString setRaw){raw = setRaw;}
+     void SetValue(int setValue){value = setValue;}
+     void SetTimeStamp(int setTimeStamp){timestamp = setTimeStamp;}
+
+     CanData();
+
+
 
 };
 
