eb9e46c7f1841bc8745cfad4a65e21fc8c6a8680
robin commeyne <robin.commeyne@telenet.be>
2017-05-15 14:58:08 +0200

    add canvisualizer

---
+++ canvisualizer/canvisualizer/www/scripts/bluetooth.js
@@ -0,0 +1,9 @@
+﻿//Handle all functions for bluetooth
+
+document.addEventListener('deviceready', function () {
+
+    new Promise(function (resolve) {
+        bluetooth.initialize(resolve: true, statusReceiver: false});
+}).then(initializeSuccess, handleError);
+});
+
