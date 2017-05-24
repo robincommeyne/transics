3090cf0d17f07ba7b4ab983ecafb66cee611d05f
robin commeyne <robin.commeyne@telenet.be>
2017-05-24 15:58:50 +0200

    Canvisualizer Bluetooth and QRCode added

---
+++ CanVisualizer/CanVisualizer/www/scripts/qrcode.js
@@ -0,0 +1,23 @@
+﻿/*
+    Handles QR code reading and set macAddress from the raspberry pi
+*/
+var qrcode = {
+    scan: function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                if (!result.cancelled) {
+                    if (result.format == "QR_CODE") {
+                        alert("We got a QR code\n" +
+                              "Result: " + result.text + "\n" +
+                              "Format: " + result.format + "\n" +
+                              "Cancelled: " + result.cancelled);
+                        bluetooth.macAddress = result.text.toString();
+                    }
+                }
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    }
+};
--- canvisualizer/canvisualizer/www/scripts/bluetooth.js
+++ canvisualizer/canvisualizer/www/scripts/bluetooth.js
@@ -1,1 +1,106 @@
 ﻿
+var bluetooth = {
+    macAddress: "00:00:00:00:00:00",  // get your mac address from bluetoothSerial.list
+    chars: "",
+
+    /*
+        Connects if not connected, and disconnects if connected:
+    */
+    manageConnection: function () {
+
+        // connect() will get called only if isConnected() (below)
+        // returns failure. In other words, if not connected, then connect:
+        var connect = function () {
+            // if not connected, do this:
+            // clear the screen and display an attempt to connect
+            bluetooth.clear();
+            bluetooth.display("Attempting to connect. " +
+                "Make sure the serial port is open on the target device.");
+            // attempt to connect:
+            bluetoothSerial.connect(
+                bluetooth.macAddress,  // device to connect to
+                bluetooth.openPort,    // start listening if you succeed
+                bluetooth.showError    // show the error if you fail
+            );
+        };
+
+        // disconnect() will get called only if isConnected() (below)
+        // returns success  In other words, if  connected, then disconnect:
+        var disconnect = function () {
+            bluetooth.display("attempting to disconnect");
+            // if connected, do this:
+            bluetoothSerial.disconnect(
+                bluetooth.closePort,     // stop listening to the port
+                bluetooth.showError      // show the error if you fail
+            );
+        };
+
+        // here's the real action of the manageConnection function:
+        bluetoothSerial.isConnected(disconnect, connect);
+    },
+    /*
+        subscribes to a Bluetooth serial listener for newline
+        and changes the button:
+    */
+    openPort: function () {
+        // if you get a good Bluetooth serial connection:
+        bluetooth.display("Connected to: " + bluetooth.macAddress);
+
+        bluetoothSerial.write("Het is gelukt!");
+        // change the button's name:
+        connectButton.innerHTML = "Disconnect";
+        // set up a listener to listen for newlines
+        // and display any new data that's come in since
+        // the last newline:
+        bluetoothSerial.subscribe('\n', function (data) {
+            bluetooth.clear();
+            bluetooth.display(data);
+        });
+    },
+
+    /*
+        unsubscribes from any Bluetooth serial listener and changes the button:
+    */
+    closePort: function () {
+        // if you get a good Bluetooth serial connection:
+        bluetooth.display("Disconnected from: " + bluetooth.macAddress);
+        // change the button's name:
+        connectButton.innerHTML = "Connect";
+        // unsubscribe from listening:
+        bluetoothSerial.unsubscribe(
+                function (data) {
+                    bluetooth.display(data);
+                },
+                bluetooth.showError
+        );
+    },
+    /*
+        appends @error to the message div:
+    */
+    showError: function (error) {
+        bluetooth.display(error);
+    },
+
+    /*
+        appends @message to the message div:
+    */
+    display: function (message) {
+        var display = document.getElementById("message"), // the message div
+            lineBreak = document.createElement("br"),     // a line break
+            label = document.createTextNode(message);     // create the label
+
+        display.appendChild(lineBreak);          // add a line break
+        display.appendChild(label);              // add the message node
+    },
+    /*
+        clears the message div:
+    */
+    clear: function () {
+        var display = document.getElementById("message");
+        display.innerHTML = "";
+    },
+
+    sendMessage: function () {
+        bluetoothSerial.write("test");
+    }
+};      // end of app
